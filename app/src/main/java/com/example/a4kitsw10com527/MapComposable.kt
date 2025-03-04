2456b08f6ec648e4c6336939dae691d9c4ae2377
4kitsw10 <4kitsw10@solent.ac.uk>
2025-03-04 14:13:01 +0000

    MapComposable redefined

--- app/src/main/java/com/example/a4kitsw10com527/MapComposable.kt
+++ app/src/main/java/com/example/a4kitsw10com527/MapComposable.kt
@@ -5,12 +5,60 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableDoubleStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.unit.dp
+import org.maplibre.android.geometry.LatLng
+import org.maplibre.android.maps.Style
+import org.ramani.compose.CameraPosition
+import org.ramani.compose.MapLibre
 
 @Composable
 fun MapComposable(modifier: Modifier) {
+    var latitude by remember { mutableDoubleStateOf(51.05) }
+    var longitude by remember { mutableDoubleStateOf(-0.72) }
+    var zoom by remember { mutableDoubleStateOf(14.0) }
+
     Surface(modifier) {
         Column {
-            Text("this is the map composable")
+            Row {
+                TextField(value = latitude.toString(), onValueChange = {
+                    latitude = it.toDouble()
+                })
+
+                TextField(value = longitude.toString(), onValueChange = {
+                    longitude = it.toDouble()
+                })
+            }
+
+            MapLibre(
+                modifier = Modifier.width(250.dp).height(250.dp),
+                styleBuilder = Style.Builder().fromUri("https://tiles.openfreemap.org/styles/bright"),
+                cameraPosition = CameraPosition(target = LatLng(latitude, longitude), zoom = zoom)
+            )
+
+            Row {
+                Button(onClick = {
+                    zoom--
+                }) {
+                    Text("-")
+                }
+
+                Text(zoom.toString())
+
+                Button(onClick = {
+                    zoom++
+                }) {
+                    Text("+")
+                }
+            }
         }
     }
 }
