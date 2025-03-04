75b053c94ce8d82283c5fc0b30283cd7102b46b0
4kitsw10 <4kitsw10@solent.ac.uk>
2025-03-04 13:52:09 +0000

    MapComposable defined

--- app/src/main/java/com/example/a4kitsw10com527/MapComposable.kt
+++ app/src/main/java/com/example/a4kitsw10com527/MapComposable.kt
@@ -1,13 +1,16 @@
 package com.example.a4kitsw10com527
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @Composable
 fun MapComposable(modifier: Modifier) {
-    Column(modifier) {
-        Text("this is the map composable")
+    Surface(modifier) {
+        Column {
+            Text("this is the map composable")
+        }
     }
 }
